79ac34c79324873d0a37d1ef68b2887f27f2880f
Vasilis Papamoustos <vpapamoustos@gmail.com>
2026-06-29 20:50:26 +0300

    initial mobile list from G training completed

--- src/changelog.js
+++ src/changelog.js
@@ -1,6 +1,12 @@
-export const APP_VERSION = "0.1a";
+export const APP_VERSION = "0.2a";
 export const APP_CHANGELOG = [
   {
+    version: "0.2a",
+    changes: [
+      "Initial Mobile list added.",
+      "Initial data import is not complete."
+    ]
+  },{
     version: "0.1a",
     changes: [
       "Initial Mobile list added.",

a08e9c4809e6e0ad6857e6cad6773e241aa9a61a
Vasilis Papamoustos <vpapamoustos@gmail.com>
2026-06-29 21:04:16 +0300

    updated changelog

--- src/changelog.js
+++ src/changelog.js
@@ -3,8 +3,9 @@ export const APP_CHANGELOG = [
   {
     version: "0.2a",
     changes: [
-      "Initial Mobile list added.",
-      "Initial data import is not complete."
+      "Initial Mobile list updated.",
+      "The deck includes only mobiles available in germanos.gr website.",
+      "All mobile attributes have been validated through germanos.gr website or GSMarena."
     ]
   },{
     version: "0.1a",
